fec513897be99ffc6a484837cdceb78c0c27cfd3
Ummadisetti Sindhuja <ummadisettisindhuja2@gmail.com>
2026-09-05 20:43:26 -0500

    Add Terraform input variables

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The GCP zone for the Compute Engine instance"
+  type        = string
+  default     = "us-central1-a"
+}
