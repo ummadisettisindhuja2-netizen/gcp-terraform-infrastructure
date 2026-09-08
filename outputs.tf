8d354e34b7cbfbd9a544c5b17a6f6c366df958af
Ummadisetti Sindhuja <ummadisettisindhuja2@gmail.com>
2026-09-07 20:35:29 -0500

    Complete Terraform output definitions

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
-output "instance_name" {
+output "project_id" {
+  description = "The GCP project ID"
+  value       = var.project_id
+}
+
+output "region" {
+  description = "The GCP region"
+  value       = var.region
+}
+
+output "zone" {
+  description = "The Compute Engine zone"
+  value       = var.zone
+}
